cb4a0ed92092f611c0b5894589ed32ceda17eef5
Salomão Neto <salomaosnff@yahoo.com>
2017-06-19 04:52:23 -0300

    Build feito em 19/06/2017 ás 04:51:45 Reset src/index.js to 9a95f44848d7875d0368651aefa092e0e58b8618.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,38 @@
+// Directives
+import Media from './directives/media';
 
+// Components
+import Grid from './components/grid/';
+import Container from './components/container/';
+import Icon from './components/icon/';
+import Button from './components/button/';
 
-export default {a: 1};
+const Plugin = {
+    // Directives
+    Media,
+
+    // Components
+    Grid,
+    Container,
+    Icon,
+    Button
+};
+
+Plugin.install = (Vue, options) => {
+
+    options = Object.assign({
+        icon: {
+            classFormat: 'mdi mdi-%',
+            inContent: false
+        }
+    }, options);
+
+    for (let c in Plugin) {
+        if (c === 'install') {
+            continue;
+        }
+        Vue.use(Plugin[c], options);
+    }
+};
+
+export default Plugin;
