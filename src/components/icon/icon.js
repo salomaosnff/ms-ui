97e6a94b3c449742a37997c622499aa4c29d27fa
Adrian Cerbaro <adriancerbaro@gmail.com>
2017-06-30 23:50:02 -0300

    Build feito em 30/06/2017 ás 23:50:02 Bug nos icones corrigido

--- src/components/icon/icon.js
+++ src/components/icon/icon.js
@@ -6,21 +6,19 @@ export default {
 
     mixins: [BindEvents, Themeable],
     render(create, {props, data, children}) {
-        
         const icon = children[0].text;
         const iconClass = props.classFormat.replace(/%/g, icon);
+
+        data.staticClass = data.staticClass ? `${iconClass} ${data.staticClass} ` : `${iconClass} `
+
         let content = [];
 
-        const classes = typeof data.class === 'string' ? data.class :
-                        Object.keys(data.class)
-                        .filter(c => data.class[c]).join(' ');
-
-        data.class = {
+        const classes = {
             'icon--dark': props.dark || !props.light,
-            'icon--light': props.light || !props.dark,
-            [iconClass]: true,
-            [classes]: true
+            'icon--light': props.light || !props.dark
         };
+
+        data.staticClass += Object.keys(classes).filter(k => classes[k]).join(' ');
 
         if(props.inContent) {
             content = icon;
