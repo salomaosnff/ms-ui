a3bf906927bdb9a52c4254afff74307e91b4e848
Adrian Cerbaro <adriancerbaro@gmail.com>
2017-06-30 23:31:43 -0300

    Build feito em 30/06/2017 ás 23:31:43 Flex auto adicionado

--- src/components/grid/flex/flex.vue
+++ src/components/grid/flex/flex.vue
@@ -12,12 +12,14 @@
             sm: String,
             md: String,
             lg: String,
-            xl: String
+            xl: String,
+            auto: Boolean
         },
         computed: {
             classes() {
                 return [
                     'ms-flex',
+                    {'ms-flex--auto': this.auto},
                     {[`ms-flex--xs-${this.xs}`]: +this.xs > 0},
                     {[`ms-flex--sm-${this.sm}`]: +this.sm > 0},
                     {[`ms-flex--md-${this.md}`]: +this.md > 0},
